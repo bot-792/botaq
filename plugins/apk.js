9a04d4fbaebed8db5f5cc70696120d0a5f8f5a51
adam.__.98 <khadabfest@gmail.com>
2026-08-23 19:39:40 +0100

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -72,7 +72,7 @@ async function sendApk(conn, m, app) {
 
     await m.react('⬇️').catch(() => {})
 
-    const apkCaption = `*🔖اسـم تطبـيق :* ${app.name}\n\n*🇲🇦 بـواسـطـة : ${BOT_NAME}*\n*👑 الـمطــور : adam.__.98*`
+    const apkCaption = `*🔖اسـم تطبـيق :* ${app.name}\n\n*🇲🇦 بـواسـطـة : ${BOT_NAME}*\n*👑 الـمطــور : mysto off *`
 
     await conn.sendMessage(m.chat, {
         document: { url: downloadUrl },
