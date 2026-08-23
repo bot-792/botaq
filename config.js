18aa62a2e345619bc0355da285a9de4f1dbc25e7
adam.__.98 <khadabfest@gmail.com>
2026-08-23 19:30:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ import { watchFile, unwatchFile } from 'fs';
 import chalk from 'chalk';
 import { fileURLToPath } from 'url';
 
-global.pairingNumber = 212715597986;
-global.owner = [['212715597986', 'Agus', true]];
+global.pairingNumber = 21271597986;
+global.owner = [['21715597986', 'Agus', true]];
 
 global.namebot = 'ChiiBOT - MD';
 global.author = 'Agus';
