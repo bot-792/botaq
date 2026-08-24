af57c44f235c7f4016ebc1ab6e7b37a3873b1211
mysto off <khadabfest@gmail.com>
2026-08-24 16:34:18 +0100

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -8,7 +8,7 @@ import axios from "axios";
 const channelName = '𝗝𝗜𝗧𝗢𝗦𝗦𝗔 𝗕𝗢𝗧 🇲🇦'
 const CHANNEL_ID = '120363410733859643@newsletter'
 const INSTAGRAM_URL = `https://instagram.com/mysto__off`
-const DEVELOPER = '*𝗕𝗬 𝗠𝗬𝗦𝗧𝗢 𝗢𝗙𝗙*'
+const DEVELOPER = '*𝗠𝗬𝗦𝗧𝗢 𝗢𝗙𝗙*'
 const newsletter = {
     forwardingScore: 999,
     isForwarded: true,
